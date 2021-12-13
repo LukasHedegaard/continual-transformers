3f4b2768e5e53a1d8c550521ad9bd3228dcee967
LukasHedegaard <lh@eng.au.dk>
2021-12-13 10:46:17 +0100

    Add CoReSiTransformerEncoder to __init__

--- continual_transformers/__init__.py
+++ continual_transformers/__init__.py
@@ -3,3 +3,4 @@ from .co_re_mha import CoReMultiheadAttention  # noqa: F401
 from .co_re_trans_enc import CoReTransformerEncoder  # noqa: F401
 from .co_si_mha import CoSiMultiheadAttention  # noqa: F401
 from .co_si_trans_enc import CoSiTransformerEncoder  # noqa: F401
+from .co_resi_trans_enc import CoReSiTransformerEncoder  # noqa: F401
